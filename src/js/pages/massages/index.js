0a36e2363de2ecd143d7476499791aeaf872d823
Yury Demidov <demidov_uriy@poisondrop.ru>
2020-10-28 02:55:56 +0300

    Add yandex map

--- src/js/pages/massages/index.js
+++ src/js/pages/massages/index.js
@@ -4,3 +4,4 @@ import '../../modules/applicationForm';
 import '../../modules/reviewsBlock';
 import '../../modules/priceListForm';
 import '../../modules/popups/popupAppointment';
+import '../../modules/map/map';
