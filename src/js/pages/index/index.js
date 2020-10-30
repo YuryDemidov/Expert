aac0c2f8ec4d6ae58ed49dd94f2fd3f2e90f2cca
Yury Demidov <demidov_uriy@poisondrop.ru>
2020-10-30 13:24:13 +0300

    Add famous-clients slider. Add reviews slider autoplay

--- src/js/pages/index/index.js
+++ src/js/pages/index/index.js
@@ -4,6 +4,7 @@ import '../../modules/reviewsBlock';
 import '../../modules/applicationForm';
 import '../../modules/massageTypesBlock';
 import '../../modules/massageChoosingTest/massageChoosingTest';
+import '../../modules/famousClientsSlider';
 import '../../modules/videoPlayers';
 import '../../modules/priceListForm';
 import '../../modules/popups/popupAppointment';
