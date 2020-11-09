c9f4312afcc8418b222566a1aa892aad29fe2ed9
Yury Demidov <demidov_uriy@poisondrop.ru>
2020-11-09 06:24:36 +0300

    Add massage results on massage pages

--- src/js/modules/massageResults/backResultsSlider.js
+++ src/js/modules/massageResults/backResultsSlider.js
@@ -2,22 +2,31 @@ import { tns } from 'tiny-slider/src/tiny-slider';
 import { GLOBAL_VARS } from '../../utils/constants/globalVars';
 
 const backResultsWrap = document.querySelector(`.back-results`);
-const backResultsSliderNode = backResultsWrap.querySelector(`.back-results__slider`);
-const prevButton = backResultsWrap.querySelector(`.slider__button_prev`);
-const nextButton = backResultsWrap.querySelector(`.slider__button_next`);
 let backResultsSlider;
 
-if (globalThis.innerWidth < GLOBAL_VARS.breakpoints.tabletMinWidth) {
-  backResultsSlider = tns({
-    container: backResultsSliderNode,
-    prevButton: prevButton,
-    nextButton: nextButton,
-    nav: true,
-    navPosition: `bottom`,
-    loop: true,
-    mouseDrag: true,
-    autoHeight: true
-  });
+initSlider();
+
+function initSlider() {
+  if (!backResultsWrap) {
+    return;
+  }
+
+  const backResultsSliderNode = backResultsWrap.querySelector(`.back-results__slider`);
+  const prevButton = backResultsWrap.querySelector(`.slider__button_prev`);
+  const nextButton = backResultsWrap.querySelector(`.slider__button_next`);
+
+  if (globalThis.innerWidth < GLOBAL_VARS.breakpoints.tabletMinWidth) {
+    backResultsSlider = tns({
+      container: backResultsSliderNode,
+      prevButton: prevButton,
+      nextButton: nextButton,
+      nav: true,
+      navPosition: `bottom`,
+      loop: true,
+      mouseDrag: true,
+      autoHeight: true
+    });
+  }
 }
 
 export { backResultsSlider };
--- src/js/modules/massageResults/figureResultsSlider.js
+++ src/js/modules/massageResults/figureResultsSlider.js
@@ -1,27 +1,37 @@
 import { tns } from 'tiny-slider/src/tiny-slider';
 
 const figureResultsSliderWrap = document.querySelector(`.figure-results__slider`);
-const figureResultsSliderNode = figureResultsSliderWrap.querySelector(`.figure-results__slides`);
-const prevButton = figureResultsSliderWrap.querySelector(`.slider__button_prev`);
-const nextButton = figureResultsSliderWrap.querySelector(`.slider__button_next`);
+let figureResultsSlider;
 
-const figureResultsSlider = tns({
-  container: figureResultsSliderNode,
-  prevButton: prevButton,
-  nextButton: nextButton,
-  mouseDrag: true,
-  autoHeight: true,
-  responsive: {
-    0: {
-      speed: 500,
-      nav: true,
-      navPosition: `bottom`
-    },
-    1024: {
-      speed: 1200,
-      nav: false
-    }
+initSlider();
+
+function initSlider() {
+  if (!figureResultsSliderWrap) {
+    return;
   }
-});
+
+  const figureResultsSliderNode = figureResultsSliderWrap.querySelector(`.figure-results__slides`);
+  const prevButton = figureResultsSliderWrap.querySelector(`.slider__button_prev`);
+  const nextButton = figureResultsSliderWrap.querySelector(`.slider__button_next`);
+
+  figureResultsSlider = tns({
+    container: figureResultsSliderNode,
+    prevButton: prevButton,
+    nextButton: nextButton,
+    mouseDrag: true,
+    autoHeight: true,
+    responsive: {
+      0: {
+        speed: 500,
+        nav: true,
+        navPosition: `bottom`
+      },
+      1024: {
+        speed: 1000,
+        nav: false
+      }
+    }
+  });
+}
 
 export { figureResultsSlider };
