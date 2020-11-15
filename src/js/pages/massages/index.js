16771fd03c4b98c63dd51e6e8f1c491a65895506
Yury Demidov <demidov_uriy@poisondrop.ru>
2020-11-16 00:59:33 +0300

    Finish faq block. Add massage-description photos

--- src/js/pages/massages/index.js
+++ src/js/pages/massages/index.js
@@ -4,6 +4,7 @@ import '../../modules/applicationForm';
 import '../../modules/massageResults/sliders';
 import '../../modules/reviewsBlock';
 import '../../modules/video/massageVideo';
+import '../../modules/faqBlock';
 import '../../modules/priceListForm';
 import '../../modules/popups/popupAppointment';
 import '../../modules/map/map';
