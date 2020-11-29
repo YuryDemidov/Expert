9cd62811a6012a6b335973cc7325f4c98934cec9
Yury Demidov <demidov_uriy@poisondrop.ru>
2020-11-29 07:04:36 +0300

    About page. Add rest articles on articles page

--- src/js/pages/service/about/index.js
+++ src/js/pages/service/about/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+import { DROPDOWN_STATES } from '../../../utils/constants/enums/dropdownStates';
+
+document.querySelectorAll(`.dropdown[data-on=click]`).forEach(dropdown => {
+  dropdown.addEventListener(`click`, dropdownToggleHandler);
+})
+
+function dropdownToggleHandler(evt) {
+  if (evt.target.classList.contains(`dropdown__trigger`)) {
+    const dropdown = evt.target.closest(`.dropdown`);
+
+    evt.preventDefault();
+    dropdown.dataset.state = dropdown.dataset.state === DROPDOWN_STATES.closed ? DROPDOWN_STATES.opened : DROPDOWN_STATES.closed;
+  }
+}
